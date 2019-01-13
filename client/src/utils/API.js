469b42741ce680999e484946a221079ce8293b8e
Steve N <nguy.steve@gmail.com>
2019-01-12 19:55:55 -0800

    code clean up

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -80,29 +80,29 @@ export default {
   acctmonth: function() {
     return axios.get("/api/transactions/acctmonth");
   },
-  // sums data by account details and month
-  compareyear: function() {
-    return axios.get("/api/transactions/compareyear");
-  },
-  // sums data by account details and month
-  comparequarter: function() {
-    return axios.get("/api/transactions/comparequarter");
-  },
-  // sums data by account details and month
-  comparemonth: function() {
-    return axios.get("/api/transactions/comparemonth");
-  },
-  compareyrsum: function() {
-    return axios.get("/api/transactions/compareyrsum");
-  },
-  // sums data by account details and month
-  compareqtrsum: function() {
-    return axios.get("/api/transactions/compareqtrsum");
-  },
-  // sums data by account details and month
-  comparemthsum: function() {
-    return axios.get("/api/transactions/comparemthsum");
-  },
+  // // sums data by account details and month
+  // compareyear: function() {
+  //   return axios.get("/api/transactions/compareyear");
+  // },
+  // // sums data by account details and month
+  // comparequarter: function() {
+  //   return axios.get("/api/transactions/comparequarter");
+  // },
+  // // sums data by account details and month
+  // comparemonth: function() {
+  //   return axios.get("/api/transactions/comparemonth");
+  // },
+  // compareyrsum: function() {
+  //   return axios.get("/api/transactions/compareyrsum");
+  // },
+  // // sums data by account details and month
+  // compareqtrsum: function() {
+  //   return axios.get("/api/transactions/compareqtrsum");
+  // },
+  // // sums data by account details and month
+  // comparemthsum: function() {
+  //   return axios.get("/api/transactions/comparemthsum");
+  // },
   preparer: function() {
     return axios.get("/api/transactions/preparer")
   }
