e8b8f46a7e3a4472af1bfdccb3573fc404f9fbb3
Mearat Hou <mearatjames@gmail.com>
2019-01-13 14:18:20 -0800

    Padding bottom fixed

--- client/src/pages/Reports.js
+++ client/src/pages/Reports.js
@@ -55,7 +55,7 @@ const styles = theme => ({
   content: {
     flexGrow: 1,
     padding: theme.spacing.unit * 3,
-    paddingBottom: '20px',
+    paddingBottom: '80px',
   },
   toolbar: theme.mixins.toolbar,
   drawerPaper: {
