e09558d9aa745dc55c529059ed83dc88f13e252c
Steve N <nguy.steve@gmail.com>
2019-01-17 21:07:31 -0800

    git push deploy

---
+++ client/build/precache-manifest.c2b47f7dbd81632f17d2cc00dcdfe8f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "78feead2f1d8d1b25895",
+    "url": "/static/css/main.6c5468bf.chunk.css"
+  },
+  {
+    "revision": "78feead2f1d8d1b25895",
+    "url": "/static/js/main.78feead2.chunk.js"
+  },
+  {
+    "revision": "27caddf03e392703b69f",
+    "url": "/static/js/1.27caddf0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ecc8830fb0ba928dbdd7d8df22345c33",
+    "url": "/index.html"
+  }
+];
